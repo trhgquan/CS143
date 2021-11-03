7553f33efe79bce6814b0732a7b552a117eb567b
trhgquan <quanthcstpbt@gmail.com>
2021-11-03 18:37:02 +0700

    Update parameter

--- Lab-01/main.py
+++ Lab-01/main.py
@@ -12,9 +12,9 @@ from utility import *
 ###############################
 
 def main(argv):
-    input_file, output_file = '', ''
+    input_file, output_file, algorithm_used = '', '', ''
     try:
-        opts, args = getopt.getopt(argv, 'hi:o', ['input=', 'output='])
+        opts, args = getopt.getopt(argv, 'hi:o', ['input=', 'output=', 'algo='])
     except getopt.GetoptError:
         print('Command line arguments invalid, try again.')
         sys.exit(2)
@@ -24,8 +24,10 @@ def main(argv):
             input_file = arg
         elif opt == '--output':
             output_file = arg
+        elif opt == '--algo':
+            algorithm_used = arg
 
-    if (input_file == '' or output_file == ''):
+    if (input_file == '' or output_file == '' or algorithm_used == ''):
         print('Arguments missing, try again.')
         sys.exit(2)
 
@@ -34,8 +36,18 @@ def main(argv):
 
     route = []
     # BO SUNG HAM GOI THUAT TOAN O DAY
-
-
+    if algorithm_used == 'dfs':
+        pass
+    elif algorithm_used == 'bfs':
+        pass
+    elif algorithm_used == 'greedy':
+        pass
+    elif algorithm_used == 'astar':
+        pass
+    elif algorithm_used == 'bonus':
+        pass
+    else:
+        print('Algorithm not specified.')
 
     ##################################
 
