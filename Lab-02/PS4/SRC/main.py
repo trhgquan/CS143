933998a76d7631bfc7a750a78d2f6802aa56ad58
trhgquan <quanthcstpbt@gmail.com>
2021-12-09 00:48:42 +0700

    Second implementation, almost finished!

--- Lab-02/PS4/SRC/main.py
+++ Lab-02/PS4/SRC/main.py
@@ -1,164 +1,28 @@
-import sys
-import collections
+from os import walk
+from resolution import logic_resolution
 
-LOGICAL_EXPRESSIONS = ['OR']
-
-output_clauses = []
-finished = False
-
-def preprocess(phrase : str) -> list:
-    '''Split a phrase into literals
-
-    Args:
-        phrase (str) : phrase to split.
-    
-    Returns:
-        list: splitted list of literals.
-    '''
-    phrase = phrase.split(' ')
-
-    preprocessed_ = []
-
-    for literal in phrase:
-        if literal not in LOGICAL_EXPRESSIONS:
-            preprocessed_.append(literal)
-    return preprocessed_
-
-def inverse_literal(x : str) -> str:
-    '''Inverting a literal.
-
-    Args:
-        x (str) : literal to invert.
-    
-    Returns:
-        str : inverted literal.
-    '''
-    if '-' in x:
-        return x.replace('-', '')
-    return '-' + x
-
-def is_inverse(x1 : str, x2 : str) -> bool:
-    '''Check if a literal is another literal's inverse
-
-    Args:
-        x1 (str) : first literal.
-        x2 (str) : second literal.
-    
-    Returns:
-        bool : True if both are others inverse.
-    '''
-    return inverse_literal(x1) == x2
-
-def standardlise(clause : list) -> list:
-    unique_clause = set(clause)
-    return sorted(list(unique_clause))
-
-def is_always_valid(clause : list) -> bool:
-    '''Check if this is an always-true clause.
-
-    Args:
-        - clause (list) : clause to check
-    
-    Returns:
-        - bool
-    '''
-
-    for i in range(len(clause) - 1):
-        for j in range(i + 1, len(clause)):
-            if is_inverse(clause[i], clause[j]):
-                return True
-    return False
-
-def pl_resolve(clause_a : list, clause_b : list) -> list:
-    resolvents_ = []
-    for i in range(len(clause_a)):
-        for j in range(len(clause_b)):
-            if is_inverse(clause_a[i], clause_b[j]):
-                resolvent = clause_a[:i] + clause_a[i + 1:] + clause_b[:j] + clause_b[j + 1:]
-                resolvents_.append(standardlise(resolvent))
-    return resolvents_
-
-def pl_resolution(kb : list, alpha : list) -> bool:
-    clauses_ = kb;
-
-    for literal in alpha:
-        if [literal] not in kb:
-            clauses_.append([literal])
-
-    finished = False
-
-    while True:
-        output_clauses.append([])
-        new_clauses = []
-        for i in range(len(kb) - 1):
-            for j in range(i + 1, len(kb)):
-                resolvents = pl_resolve(kb[i], kb[j])
-                if [] in resolvents:
-                    new_clauses.append([])
-                    finished = True
-                    break
-
-                for resolvent_ in resolvents:
-                    if is_always_valid(resolvent_): break
-                    if resolvent_ not in clauses_ and resolvent_ not in output_clauses[-1]:
-                        new_clauses.append(resolvent_)
-            
-            if finished: break
-
-        if len(new_clauses) == 0:
-            return False
-        
-        output_clauses[-1] = new_clauses
-        for x in new_clauses: clauses_.append(x)
-        
-        if finished:
-            return True
-
-def cnf_string_format(clauses : list) -> str:
-    '''Print the clause in CNF format.
-
-    Args:
-        - clauses (list) : List of literals forming a clause, by default is OR.
-    
-    Returns:
-        - str
-    '''
-    res_string = ''
-
-    if len(clauses) == 0:
-        res_string = '{}'
-
-    elif len(clauses) == 1:
-        res_string = clauses[0]
-
-    else:
-        for clause_ in clauses[:-1]:
-            res_string += clause_
-            res_string += ' OR '
-        res_string += clauses[-1]
-
-    return res_string
-
+INPUT_DIR = 'input/'
+OUTPUT_DIR = 'output/'
 
 def main():
-    with open('input.txt', 'r+', encoding = 'utf-8') as f:
-        alpha = preprocess(f.readline().strip())
-        alpha = [inverse_literal(l) for l in alpha]
+    # Walkthrough files inside input directory.
+    # Credit: https://stackoverflow.com/a/3207973
+    filenames = next(walk(INPUT_DIR), (None, None, []))[2]
 
-        n = int(f.readline().strip())
+    for file in filenames:
+        with open(INPUT_DIR + file, 'r+') as f:
+            alpha = f.readline().strip()
+            n = int(f.readline().strip())
+            kb = []
+            for i in range(n):
+                clause = f.readline().strip()
+                kb.append(clause)
 
-        kb_ = []
-        for i in range(0, n):
-            kb_.append(preprocess(f.readline().strip()))
-
-        result = pl_resolution(kb_, alpha)
-
-        for clause in output_clauses:
-            print(len(clause))
-            for c in clause:
-                print(cnf_string_format(c))
-        
-        print('YES') if result else print('NO')
+        resolver = logic_resolution(kb, alpha)
+        resolver.pl_resolution()
+        with open(OUTPUT_DIR + file, 'w+') as f:
+            resolver.print_output(f = f)
+        print('Solved:', file)
 
 if __name__ == '__main__':
     main()
--- Lab-02/PS4/SRC/resolution.py
+++ Lab-02/PS4/SRC/resolution.py
@@ -1,14 +1,23 @@
-from PS4.SRC.main import inverse_literal, is_inverse
-
+from io import FileIO
 
 class logic_resolution:
     LOGICAL_EXPRESSIONS = ['OR']
 
     def __init__(self, kb : list, alpha : list) -> None:
-        self.__kb = kb
-        self.__alpha = alpha
         self.__has_solution = False
         self.__output_clauses = []
+        self.__kb = []
+
+        # Preprocessing KB
+        for clause in kb:
+            self.__kb.append(self.preprocess(clause.strip()))
+
+        # Get inverse of alpha clauses.
+        # And insert them into knowledge base KB.
+        alpha = [self.inverse_literal(a) for a in self.preprocess(alpha)]
+        for literal in alpha:
+            if [literal] not in kb:
+                self.__kb.append(self.standardlise([literal]))
     
     @staticmethod
     def preprocess(clause : str) -> list:
@@ -24,7 +33,7 @@ class logic_resolution:
         preprocessed_ = []
         
         for literal in clause.split(' '):
-            if literal not in logic_resolution.LOGICAL_EXPRESSIONS:
+            if len(literal) > 0 and literal not in logic_resolution.LOGICAL_EXPRESSIONS:
                 preprocessed_.append(literal)
         
         return preprocessed_
@@ -54,7 +63,7 @@ class logic_resolution:
         Returns:
             - bool
         '''
-        return inverse_literal(literal_a) == literal_b
+        return logic_resolution.inverse_literal(literal_a) == literal_b
 
     @staticmethod
     def is_always_valid(clause : list) -> bool:
@@ -68,6 +77,120 @@ class logic_resolution:
         '''
         for i in range(len(clause) - 1):
             for j in range(i + 1, len(clause)):
-                if (is_inverse(clause[i], clause[j])):
+                if (logic_resolution.is_inverse(clause[i], clause[j])):
                     return True
         return False
+    
+    @staticmethod
+    def standardlise(clause : list) -> list:
+        '''Sorting a clause.
+
+        Args:
+            - clause (list) : clause to sort.
+        
+        Returns:
+            - list
+        '''
+        # Removing duplicates from clause.
+        unique = set(clause)
+
+        # Sort clauses, ignoring the sign before (-A and A will be treated the same).
+        return sorted(list(unique), key = lambda x: x[-1])
+
+    @staticmethod
+    def cnf_format(clause : list) -> list:
+        '''Print the clause in CNF format.
+
+        Args:
+            - clauses (list) : List of literals forming a clause, by default is OR.
+        
+        Returns:
+            - str
+        '''
+        res_string = ''
+
+        if len(clause) == 0:
+            res_string = '{}'
+        
+        elif len(clause) == 1:
+            res_string = clause[0]
+
+        else:
+            for literal in clause[:-1]:
+                res_string += literal
+                res_string += ' OR '
+            res_string += clause[-1]
+        
+        return res_string
+
+    @staticmethod
+    def pl_resolve(clause_a : list, clause_b : list) -> list:
+        '''Resolve two clause.
+
+        Args:
+            - clause_a (list) : first clause.
+            - clause_b (list) : second clause.
+
+        Returns:
+            - list
+        '''
+        resolvents_ = []
+
+        for i in range(len(clause_a)):
+            for j in range(len(clause_b)):
+                if logic_resolution.is_inverse(clause_a[i], clause_b[j]):
+                    resolvent = clause_a[:i] + clause_a[i + 1:] + clause_b[:j] + clause_b[j + 1:]
+                    resolvents_.append(logic_resolution.standardlise(resolvent))
+
+        return resolvents_
+
+    def pl_resolution(self) -> bool:
+        '''Resolve given KB and alpha.
+
+        Args:
+            - self
+        
+        Returns:
+            - bool
+        '''
+        while True:
+            self.__output_clauses.append([])
+            new_clauses = []
+
+            for i in range(len(self.__kb) - 1):
+                for j in range(i + 1, len(self.__kb)):
+                    resolvents = self.pl_resolve(self.__kb[i], self.__kb[j])
+
+                    if [] in resolvents:
+                        new_clauses.append([])
+                        self.__has_solution = True
+                    
+                    for resolvent_ in resolvents:
+                        if self.is_always_valid(resolvent_):
+                            continue
+                        if resolvent_ not in self.__kb and resolvent_ not in self.__output_clauses[-1]:
+                            new_clauses.append(resolvent_)
+            
+            if len(new_clauses) == 0:
+                self.__has_solution = False
+                return False
+
+            for x in new_clauses:
+                if x not in self.__kb: self.__kb.append(x)
+                if x not in self.__output_clauses[-1]: self.__output_clauses[-1].append(x)
+
+            if self.__has_solution:
+                return True
+    
+    def print_output(self, f : FileIO = None) -> None:
+        '''Print output to a readable form.
+
+        Args:
+            - f (FileIO) : file handle to print.
+        '''
+        for clause in self.__output_clauses:
+            print(len(clause), file = f)
+            for c in clause:
+                print(self.cnf_format(c), file = f)
+        
+        print('YES', file = f) if self.__has_solution else print('NO', file = f)
