7e36108400ee4526d9c7ccb35fb450e977239b77
hwangswan <mrthedzam@gmail.com>
2021-12-24 14:44:02 +0700

    Added ID3Node struct

--- Lab-03/main.jl
+++ Lab-03/main.jl
@@ -21,11 +21,34 @@ function splitdf(df, pct)
   return view(df, sel, :), view(df, .!sel, :)
 end
 
+struct ID3_Node
+  ids::Int64
+  entropy::Float64
+  depth::Int64
+  split_attribute::Bool
+  children::Vector{ID3_Node}
+  order::Int64
+  label::String
+
+  function set_props(split_attribute_, order_)
+    split_attribute = split_attribute_
+    order = order_
+  end
+
+  function set_label(label_)
+    label = label_
+  end
+end
+
+function entropy(freq)
+  freq_0 = freq[]
+end
+
 # Loading dataset
 raw_df = DataFrame(CSV.File("iris.csv"))
 
-# Split dataset to training (80%) and testing (20%)
-train_df, test_df = splitdf(raw_df, 0.66666666)
+# Split dataset to training (2/3) and testing (1/3)
+train_df, test_df = splitdf(raw_df, 0.666666667)
 
 X_train, X_test = train_df[!, Not("variety")], test_df[!, Not("variety")]
 y_train, y_test = train_df[!, "variety"], test_df[!, "variety"]
@@ -33,7 +56,7 @@ y_train, y_test = train_df[!, "variety"], test_df[!, "variety"]
 X_train, X_test = float.(Matrix(X_train)), float.(Matrix(X_test))
 y_train, y_test = string.(y_train), string.(y_test)
 
-
+#=
 # For testing only.
 model = DecisionTreeClassifier(max_depth = 2)
 fit!(model, X_train, y_train)
@@ -43,4 +66,4 @@ print_tree(model, 5)
 using ScikitLearn.CrossValidation: cross_val_score
 
 accuracy = cross_val_score(model, X_test, y_test, cv = 3)
-
+=#
